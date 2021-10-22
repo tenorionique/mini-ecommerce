a8733a3d36bca958ae43397baf267a2e8e11fc72
tenorionique <monique_doretto@hotmail.com>
2021-10-22 15:27:25 -0300

    feat(listar-produtos): lista os produtos e reenderiza na page mini ecommerce

---
+++ src/components/produtos/produtos.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ListarProdutos from './listar-produtos';
+
+function Produtos() {
+    return (
+        <ListarProdutos/>
+    )
+}
+
+export default Produtos;
---
+++ src/mini-ecommerce.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './mini-ecommerce.css';
+import Menu from './components/menu/menu';
+import Produtos from './components/produtos/produtos';
+import Checkout from './components/checkout/checkout';
+
+function MiniEcommerce() {
+  const [carrinho, setCarrinho] = useState({ produtos: [] });
+  const [exibirProdutos, setExibirProdutos] = useState(true);
+  const [exibirCheckout, setExibirCheckout] = useState(false);
+  const [total, setTotal] = useState('0,00');
+
+
+  return (
+    <section>
+      <Menu />
+      <Produtos />
+   
+    </section>
+  );
+}
+
+export default MiniEcommerce;
